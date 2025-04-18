166a86fc2f21524616a1d109807dbea9bb611ae6
Chinmayee Deshmukh <109342097+Chinmayee21d@users.noreply.github.com>
2025-04-18 19:37:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 from sklearn.linear_model import LinearRegression
 import numpy as np
-
+#Deveops webhooks
 # Page layout optimization
 st.set_page_config(layout="wide", page_title="Amazon Sales Dashboard")
 
